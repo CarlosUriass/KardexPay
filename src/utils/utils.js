4a26fc5ab7795f5845213f0b97b7bfebde3187a6
Adrian Tadeo <adntd24@gmail.com>
2025-03-13 14:48:36 -0700

    validate email function

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -3,4 +3,16 @@ function capitalize(str) {
   return str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
 }
 
-export default capitalize;
+
+function validateEmail(email){
+  return String(email)
+  .toLowerCase()
+  .match(
+    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+  );
+}
+
+export default{
+  capitalize,
+  validateEmail,
+}
