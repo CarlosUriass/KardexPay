899e71f6c0ba34bf198809cd9c6bb32d79556267
Carlos Urias <hicarlosurias@gmail.com>
2025-03-16 11:11:19 -0700

    use context for selected service

--- src/components/ServiceCard.jsx
+++ src/components/ServiceCard.jsx
@@ -1,7 +1,16 @@
 import { Link } from "react-router-dom";
+import { useContext } from "react";
+import { ServiceContext } from "../context/ServiceContext";
 import utils from "../utils/utils";
 
-function ServiceCard({ image, title, description }) {
+function ServiceCard({ image, title, description, precio }) {
+  const { setService } = useContext(ServiceContext); // Accede a la función para actualizar el servicio
+  const service = { image, title, description, precio }; // Crea el objeto del servicio
+
+  const handleClick = () => {
+    setService(service); // Al hacer clic, guardamos el servicio en el contexto
+  };
+
   return (
     <div className="bg-white shadow-lg rounded-lg overflow-hidden w-80 border border-gray-200">
       <img className="w-full h-48 object-cover" src={image} alt={title} />
@@ -11,10 +20,14 @@ function ServiceCard({ image, title, description }) {
         </h3>
         <p className="text-gray-600 mt-2 h-[100px] overflow-y-auto">
           {utils.capitalize(description)}
-        </p>{" "}
-        {/* Limitar altura y permitir desplazamiento */}
-        <Link to={"/tramites"}>
-          <button className="mt-4 w-full bg-blue-400 text-white py-2 rounded-md hover:bg-blue-500 transition cursor-pointer">
+        </p>
+        <p>$ {precio}</p>
+
+        <Link to="/tramites">
+          <button
+            onClick={handleClick}
+            className="mt-4 w-full bg-blue-400 text-white py-2 rounded-md hover:bg-blue-500 transition cursor-pointer"
+          >
             Tramitar ahora
           </button>
         </Link>
