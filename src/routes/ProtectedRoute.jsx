99874c673389bec8a5aeada884127480f404473d
Carlos Urias <hicarlosurias@gmail.com>
2025-03-17 13:39:33 -0700

    route /tramites protected if no exists context

--- src/routes/ProtectedRoute.jsx
+++ src/routes/ProtectedRoute.jsx
@@ -1,9 +1,17 @@
 import { Navigate, Outlet } from "react-router-dom";
+import { useContext } from "react";
+import { ServiceContext } from "../context/ServiceContext";
 
 const ProtectedRoute = () => {
-  const isAunthenticated = localStorage.getItem("token");
+  const { selectedService } = useContext(ServiceContext);
 
-  return isAunthenticated ? <Outlet /> : <Navigate to="" />;
+  // Si no hay servicio seleccionado en el contexto, redirige a /servicios
+  if (!selectedService) {
+    return <Navigate to="/servicios" />;
+  }
+
+  // Si hay un servicio seleccionado, permite acceder a la ruta
+  return <Outlet />;
 };
 
 export default ProtectedRoute;
--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { ServiceProvider } from "../context/ServiceContext";
 import LandingPage from "../pages/LandingPage";
 import Servicios from "../pages/Servicios";
 import Tramites from "../pages/Tramites";
@@ -10,23 +11,31 @@ import Dashboard from "../pages/Dashboard";
 
 function AppRoutes() {
   return (
-    <Router>
-      <Routes>
-        {/* RUTAS PUBLICAS */}
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/servicios" element={<Servicios />} />
-        <Route path="/tramites" element={<Tramites />} />
-        <Route path="/consultas" element={<Consultas />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/recuperarContraseña" element={<RecuperarContraseña />} />
+    <ServiceProvider>
+      <Router>
+        <Routes>
+          {/* RUTAS PUBLICAS */}
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/servicios" element={<Servicios />} />
+          <Route path="/consultas" element={<Consultas />} />
+          <Route path="/login" element={<Login />} />
+          <Route
+            path="/recuperarContraseña"
+            element={<RecuperarContraseña />}
+          />
 
-        {/* RUTAS PRIVADAS*/}
+          {/* RUTA PROTEGIDA: Solo accesible si hay un servicio seleccionado */}
+          <Route element={<ProtectedRoute />}>
+            <Route path="/tramites" element={<Tramites />} />
+          </Route>
 
-        <Route element={<ProtectedRoute />}>
-          <Route path="/Dashboard" element={<Dashboard />} />
-        </Route>
-      </Routes>
-    </Router>
+          {/* RUTAS PROTEGIDAS */}
+          <Route element={<ProtectedRoute />}>
+            <Route path="/Dashboard" element={<Dashboard />} />
+          </Route>
+        </Routes>
+      </Router>
+    </ServiceProvider>
   );
 }
 
