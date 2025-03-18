b4891db4b7d79ea0e0ba031500c726a400dfa670
Carlos Urias <hicarlosurias@gmail.com>
2025-03-17 18:23:38 -0700

    validate and check service before fetching student data

--- src/components/FormularioTramite.jsx
+++ src/components/FormularioTramite.jsx
@@ -1,157 +1,221 @@
 import axios from "axios";
-import { useState } from "react";
+import { useState, useContext, useEffect } from "react";
+import { ServiceContext } from "../context/ServiceContext"; // Asegúrate de que la ruta sea correcta
 import utils from "../utils/utils";
+import Modal from "./Modal";
 
 function FormularioTramite() {
+  const { selectedService } = useContext(ServiceContext);
+
+  useEffect(() => {
+    if (selectedService) {
+      setTramite(selectedService.title);
+    }
+  }, [selectedService]);
+
   const [matricula, setMatricula] = useState("");
-  const [studentData, setStudentData] = useState({
+  const [usuarioData, setUsuarioData] = useState({
     nombre: "",
     primerApellido: "",
     segundoApellido: "",
     facultad: "",
   });
+  const [tramite, setTramite] = useState("");
 
-  const fetchStudentData = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [modalMessage, setModalMessage] = useState("");
+
+  const openModal = (message) => {
+    setModalMessage(message);
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
+
+  // Verifica si el servicio es aplicable a alumnos
+  const verificarServicioAplicable = () => {
+    console.log("Verificando si el servicio es aplicable:", tramite);
+
+    axios
+      .get(`http://localhost:5001/api/servicios/aplicableAlumnos`, {
+        params: { servicio: tramite },
+      })
+      .then((response) => {
+        if (response.data.aplicable) {
+          console.log(response.data.aplicable);
+
+          fetchUsuarioData();
+        } else {
+          // Aqui buscar entre los empleados
+        }
+      })
+      .catch(() => {
+        openModal("Hubo un error al verificar si el servicio es aplicable.");
+      });
+  };
+
+  // Obtener datos del alumno
+  const fetchUsuarioData = () => {
     if (matricula.trim() !== "") {
       axios
         .get(`http://localhost:5001/api/buscarAlumno/encontraralumno`, {
-          params: { matricula }, // Enviamos la matrícula como parámetro de consulta
+          params: { matricula },
         })
         .then((response) => {
-          setStudentData(response.data);
-          console.log(response.data);
+          setUsuarioData(response.data);
         })
-        .catch((error) => {
-          console.error("Error fetching student data:", error);
+        .catch(() => {
+          openModal(
+            "No se encontró la matrícula. Verifique si el servicio seleccionado aplica para su matrícula."
+          );
         });
     }
   };
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    verificarServicioAplicable(); // Verifica si el servicio es aplicable antes de proceder
+  };
+
   return (
-    <div className="ml-10 mt-5 mb-10 shadow-2xl rounded-2xl p-5 w-full max-w-4xl bg-white relative z-20 h-150">
-      <form className="space-y-6 p-9">
-        {/* Matrícula */}
-        <div className="grid grid-cols-1 sm:grid-cols-2 gap-6 ">
-          <div>
-            <label
-              htmlFor="matricula"
-              className="block text-sm font-medium text-gray-700"
-            >
-              Matrícula
-            </label>
-            <input
-              type="text"
-              id="matricula"
-              name="matricula"
-              placeholder="Ingrese su matrícula"
-              value={matricula}
-              onChange={(e) => setMatricula(e.target.value)}
-              onBlur={fetchStudentData} // Ejecuta la petición al salir del input
-              className="mt-2 block w-full px-4 py-3 border border-gray-300 rounded-lg shadow-sm 
-              focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
-            />
-          </div>
-        </div>
+    <div className="relative">
+      {/* Modal */}
+      <Modal isOpen={isModalOpen} onClose={closeModal}>
+        <p>{modalMessage}</p>
+      </Modal>
 
-        {/* Nombre y Apellidos */}
-        <div className="grid grid-cols-1 sm:grid-cols-2 gap-6">
+      {/* Formulario con opacidad cuando el modal está abierto */}
+      <div
+        className={`ml-10 mt-5 mb-10 shadow-2xl rounded-2xl p-5 w-full max-w-4xl bg-white relative z-20 h-150 border border-gray-200 transition-opacity ${
+          isModalOpen ? "opacity-50" : "opacity-100"
+        }`}
+      >
+        <form className="space-y-6 p-9" onSubmit={handleSubmit}>
+          {/* Matrícula */}
+          <div className="grid grid-cols-1 sm:grid-cols-2 gap-6 ">
+            <div>
+              <label
+                htmlFor="matricula"
+                className="block text-sm font-medium text-gray-700"
+              >
+                Matrícula
+              </label>
+              <input
+                type="text"
+                id="matricula"
+                name="matricula"
+                placeholder="Ingrese su matrícula"
+                value={matricula}
+                onChange={(e) => setMatricula(e.target.value)}
+                onBlur={verificarServicioAplicable}
+                className="mt-2 block w-full px-4 py-3 border border-gray-300 rounded-lg shadow-sm 
+                focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
+              />
+            </div>
+          </div>
+
+          {/* Nombre y Apellidos */}
+          <div className="grid grid-cols-1 sm:grid-cols-2 gap-6">
+            <div>
+              <label
+                htmlFor="nombre"
+                className="block text-sm font-medium text-gray-700"
+              >
+                Nombre
+              </label>
+              <input
+                type="text"
+                id="nombre"
+                name="nombre"
+                placeholder="Nombre"
+                disabled
+                value={utils.capitalize(usuarioData.nombre || "")}
+                className="mt-2 block w-full max-w-lg px-4 py-3 border border-gray-300 rounded-lg shadow-sm 
+                focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
+              />
+            </div>
+
+            <div>
+              <label
+                htmlFor="primerApellido"
+                className="block text-sm font-medium text-gray-700"
+              >
+                Primer Apellido
+              </label>
+              <input
+                type="text"
+                id="primerApellido"
+                name="primerApellido"
+                placeholder="Primer apellido"
+                disabled
+                value={utils.capitalize(usuarioData.primer_apellido || "")}
+                className="mt-2 block w-full max-w-lg px-4 py-3 border border-gray-300 rounded-lg shadow-sm 
+                focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
+              />
+            </div>
+          </div>
+
+          <div className="grid grid-cols-1 sm:grid-cols-2 gap-6">
+            <div>
+              <label
+                htmlFor="segundoApellido"
+                className="block text-sm font-medium text-gray-700"
+              >
+                Segundo Apellido
+              </label>
+              <input
+                type="text"
+                id="segundoApellido"
+                name="segundoApellido"
+                placeholder="Segundo apellido"
+                disabled
+                value={utils.capitalize(usuarioData.segundo_apellido || "")}
+                className="mt-2 block w-full max-w-lg px-4 py-3 border border-gray-300 rounded-lg shadow-sm 
+                focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
+              />
+            </div>
+
+            <div>
+              <label
+                htmlFor="facultad"
+                className="block text-sm font-medium text-gray-700"
+              >
+                Facultad
+              </label>
+              <input
+                type="text"
+                id="facultad"
+                name="facultad"
+                placeholder="Facultad"
+                disabled
+                value={utils.capitalize(usuarioData.facultad || "")}
+                className="mt-2 block w-full max-w-lg px-4 py-3 border border-gray-300 rounded-lg shadow-sm 
+                focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
+              />
+            </div>
+          </div>
+
+          {/* Correo Electrónico */}
           <div>
             <label
-              htmlFor="nombre"
+              htmlFor="correoElectronico"
               className="block text-sm font-medium text-gray-700"
             >
-              Nombre
+              Correo Electrónico
             </label>
             <input
-              type="text"
-              id="nombre"
-              name="nombre"
-              placeholder="Nombre"
-              disabled
-              value={utils.capitalize(studentData.nombre)}
+              type="email"
+              id="email"
+              name="email"
+              placeholder="Ingrese su correo electrónico"
               className="mt-2 block w-full max-w-lg px-4 py-3 border border-gray-300 rounded-lg shadow-sm 
-              focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
+                focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
             />
           </div>
-
-          <div>
-            <label
-              htmlFor="primerApellido"
-              className="block text-sm font-medium text-gray-700"
-            >
-              Primer Apellido
-            </label>
-            <input
-              type="text"
-              id="primerApellido"
-              name="primerApellido"
-              placeholder="Primer apellido"
-              disabled
-              value={utils.capitalize(studentData.primer_apellido)}
-              className="mt-2 block w-full max-w-lg px-4 py-3 border border-gray-300 rounded-lg shadow-sm 
-              focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
-            />
-          </div>
-        </div>
-
-        <div className="grid grid-cols-1 sm:grid-cols-2 gap-6">
-          <div>
-            <label
-              htmlFor="segundoApellido"
-              className="block text-sm font-medium text-gray-700"
-            >
-              Segundo Apellido
-            </label>
-            <input
-              type="text"
-              id="segundoApellido"
-              name="segundoApellido"
-              placeholder="Segundo apellido"
-              disabled
-              value={utils.capitalize(studentData.segundo_apellido)}
-              className="mt-2 block w-full max-w-lg px-4 py-3 border border-gray-300 rounded-lg shadow-sm 
-              focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
-            />
-          </div>
-
-          <div>
-            <label
-              htmlFor="facultad"
-              className="block text-sm font-medium text-gray-700"
-            >
-              Facultad
-            </label>
-            <input
-              type="text"
-              id="facultad"
-              name="facultad"
-              placeholder="Facultad"
-              disabled
-              value={utils.capitalize(studentData.facultad)}
-              className="mt-2 block w-full max-w-lg px-4 py-3 border border-gray-300 rounded-lg shadow-sm 
-              focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
-            />
-          </div>
-        </div>
-
-        {/* Correo Electrónico */}
-        <div>
-          <label
-            htmlFor="correoElectronico"
-            className="block text-sm font-medium text-gray-700"
-          >
-            Correo Electrónico
-          </label>
-          <input
-            type="email"
-            id="email"
-            name="email"
-            placeholder="Ingrese su correo electrónico"
-            className="mt-2 block w-full max-w-lg px-4 py-3 border border-gray-300 rounded-lg shadow-sm 
-              focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
-          />
-        </div>
-      </form>
+        </form>
+      </div>
     </div>
   );
 }
