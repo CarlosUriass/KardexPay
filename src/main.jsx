d66e2c729dfb4fa44d3a71860208271b65bbcae5
Carlos Urias <hicarlosurias@gmail.com>
2025-04-10 09:31:20 -0700

    main & app refactor

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,9 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./app";
 
-const rootElement = document.getElementById("root");
-const root = ReactDOM.createRoot(rootElement);
-root.render(
-  <React.StrictMode>
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
     <App />
-  </React.StrictMode>
+  </StrictMode>
 );
