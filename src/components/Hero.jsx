9a168b21bb7a8dda96dc029c94bad42983c8ce27
Carlos Urias <hicarlosurias@gmail.com>
2025-04-10 09:30:04 -0700

    components for home page

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,42 +1,36 @@
-import { MdExplore } from "react-icons/md";
 import { DotLottieReact } from "@lottiefiles/dotlottie-react";
-import TramiteButton from "./TramiteButton";
+import ActionButton from "./buttons/ActionButton";
 
 function Hero() {
   return (
-    <div
-      className="h-[90vh] w-full bg-white relative 
-      before:absolute before:inset-0 before:bg-[radial-gradient(circle,rgba(0,0,0,0.1)_1px,transparent_1px)]
-      before:bg-[size:20px_20px]"
-    >
-      <div className=" ml-[120px] flex items-center justify-between">
-        {/* Texto y Botón */}
-        <div className="max-w-xl">
-          <h1 className="text-black font-bold text-5xl font-[Montserrat]">
-            Agiliza tus <br />
-            <span className="text-blue-600">trámites</span>
-          </h1>
+    <div className="h-[90vh] w-full text-[#706cec] flex items-center px-8 md:px-24 relative overflow-hidden">
+      {/* Fondo con blur dots */}
+      <div className="absolute inset-0 z-0">
+        <div className="absolute top-40 left-32 w-92 h-72 bg-indigo-300 rounded-full blur-3xl opacity-30 animate-pulse"></div>
+        <div className="absolute bottom-20 right-50 w-56 h-56 bg-purple-600 rounded-full blur-3xl opacity-30 animate-pulse"></div>
+      </div>
 
-          <h2 className="text-gray-800 font-medium leading-relaxed pt-8">
-            Agiliza tus trámites académicos de manera rápida y eficiente. <br />
-            Realiza solicitudes de constancias, kárdex y más, todo en un par de
-            clics.
-          </h2>
+      <div className="max-w-2xl relative z-10">
+        <h1 className="font-extrabold text-5xl md:text-6xl leading-tight drop-shadow-lg">
+          Agiliza tus trámites
+        </h1>
+        <p className="mt-4 text-lg md:text-xl font-medium text-black/90">
+          Gestiona tus pagos escolares de forma rápida, segura y desde cualquier
+          lugar.
+        </p>
 
-          <TramiteButton text={"Tramita Ahora"}></TramiteButton>
+        <ActionButton
+          buttonText={"Realizar un tramite"}
+          route={"/tramites"}
+        ></ActionButton>
+      </div>
 
-          <div className="pt-10">
-            <p className="font-extralight text-sm underline cursor-pointer">
-              <MdExplore className="inline-block mr-2" />
-              Conoce los trámites que puedes realizar
-            </p>
-          </div>
-        </div>
-
-        {/* Lottie Animation */}
-        <div className="w-[600px] h-[600px]">
-          <DotLottieReact src="src/assets/HeroAnimation.lottie" loop autoplay />
-        </div>
+      <div className="w-[400px] h-[600px] ml-25 relative z-10">
+        <DotLottieReact
+          src="src/assets/animations/HeroAnimation.lottie"
+          loop
+          autoplay
+        />
       </div>
     </div>
   );
