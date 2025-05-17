f15639e70971ec373c2443d4e082a94a1a2726e3
Carlos Urias <hicarlosurias@gmail.com>
2025-05-17 13:59:24 -0700

    v2

--- src/app.jsx
+++ src/app.jsx
@@ -1,27 +1,7 @@
 import "./styles/main.css";
-import routes from "./routes/routes";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        {routes.map(({ path, element, children }, index) => (
-          <Route key={index} path={path} element={element}>
-            {children &&
-              children.map((child, childIndex) => (
-                <Route
-                  key={childIndex}
-                  path={child.path}
-                  element={child.element}
-                  index={child.index}
-                />
-              ))}
-          </Route>
-        ))}
-      </Routes>
-    </BrowserRouter>
-  );
+  return;
 }
 
 export default App;
