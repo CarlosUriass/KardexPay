e243c9961a45db4e7d8427703e41e5626fbe6fcf
Carlos Urias <hicarlosurias@gmail.com>
2025-05-17 13:56:49 -0700

    v2

--- src/modules/confirmacion/pages/Confirmacion.jsx
+++
@@ -1,5 +0,0 @@
-const ConfirmacionPage = () => {
-  return <h1>Gracias por tu compra</h1>;
-};
-
-export default ConfirmacionPage;
--- src/modules/dashboard/routes/DashboardRoute.jsx
+++
@@ -1,9 +0,0 @@
-import { Navigate } from "react-router-dom";
-
-function DashboardRoute({ children }) {
-  const isAuthenticated = !!localStorage.getItem("token");
-
-  return isAuthenticated ? children : <Navigate to="/login" />;
-}
-
-export default DashboardRoute;
